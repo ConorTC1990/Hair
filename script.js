35acab2dccb0e2600c91fdec7f43db3bb40062c0
ConorTC1990 <cooney.conor17@gmail.com>
2020-02-03 22:48:35 +0000

    v1.8 testing emailJS connection

--- script.js
+++ script.js
@@ -12,5 +12,23 @@ function check(estimate) {
   document.getElementById("answer").value=estimate;
 }
 
-function imgWindow() {
-  window.open("image") }
+
+var myform = $("form#myform");
+myform.submit(function(event){
+	event.preventDefault();
+
+  // Change to your service ID, or keep using the default service
+  var service_id = "default_service";
+  var template_id = "template_ZN5QckkE";
+
+  myform.find("button").text("Sending...");
+  emailjs.sendForm(service_id,template_id,myform[0])
+  	.then(function(){ 
+    	alert("Sent!");
+       myform.find("button").text("Send");
+    }, function(err) {
+       alert("Send email failed!\r\n Response:\n " + JSON.stringify(err));
+       myform.find("button").text("Send");
+    });
+  return false;
+});
